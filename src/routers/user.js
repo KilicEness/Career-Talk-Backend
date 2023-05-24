5ef08570a8bab858107f90f84c0b09aa191bad1f
enes <eneskiliicc06@gmail.com>
2023-05-24 19:55:12 +0300

    tüm kullanıcıları alma eklendi

--- src/routers/user.js
+++ src/routers/user.js
@@ -61,6 +61,16 @@ router.get('/', auth, async (req, res) => {
     res.send(req.user)
 })
 
+//Read users
+router.get('/all', async (req, res) => {
+    try {
+        const allUsers = await User.find({})
+        res.status(200).send(allUsers)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
 //Update user
 router.patch('/', auth, async (req, res) => {
     const updates = Object.keys(req.body)
