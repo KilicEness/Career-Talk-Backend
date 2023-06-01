761039e04e23f293f6be0e0e6cd1ce00031ab070
enes <eneskiliicc06@gmail.com>
2023-06-01 14:59:56 +0300

    hata düzeltmeleri

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 const { generateMessage, generateLocationMessage } = require('./utils/messages')
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users')
 
